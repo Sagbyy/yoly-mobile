a56a55d4d548f8d653e75feff2466292d9dabf88
Sagbyy <sbouhdjeur.gagny@gmail.com>
2026-05-13 22:48:44 +0200

    Add @rn-primitives/slot dependency in package.json and update pnpm-lock.yaml. Refactor OnboardingPage component to simplify layout and remove unused styles.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,15 +4,11 @@ import "react-native-reanimated";
 import "./global.css";
 
 import { Providers } from "@/entities/providers";
-import { Text, View } from "react-native";
 
 export default function RootLayout() {
   return (
     <Providers>
       <Slot />
-      <View>
-        <Text className="text-2xl">Test de texte </Text>
-      </View>
       <StatusBar style="auto" />
     </Providers>
   );
